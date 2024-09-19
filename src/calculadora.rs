513ee8972007e8755b99cabe4d3c829ee8d7fbde
CristhianPHT <cristhian987654321p@gmail.com>
2024-09-19 09:19:35 -0500

    Agregando potencia y Raiz, incompleto

--- src/calculadora.rs
+++ src/calculadora.rs
@@ -15,6 +15,20 @@ pub fn division(x:i32, y:i32) -> Result<f32, String> {
     Ok(a/b)
   }
 }
+pub fn potencia(x:i32, y:i32) -> i64{
+  let resultado: i64 = (x as i64)**(y as i64);
+  resultado
+  // let a:i64 = x as i64; let b: i64 = y as i64;
+  // a**b
+}
+pub fn raiz(x:i32, y:i32) -> Result<f32, String> {
+  if x < 0 {
+    Err("El número no puede ser negativo.".to_string())
+  } else {
+    let a: f32 = x as f32; let b: f32 = y as f32;
+    Ok(a**(1.0/b))
+  }
+}
 
 // Módulo de pruebas
 #[cfg(test)]
@@ -40,5 +54,16 @@ mod tests {
     fn test_division() {
         assert_eq!(division(10, 2), Ok(5.0));
         assert_eq!(division(10, 0), Err("No puede dividirse con 0.".to_string()));
+    }
+
+    #[test]
+    fn test_potencia() {
+      assert_eq!(potencia(2, 3), 8);
+    }
+
+    #[test]
+    fn test_raiz() {
+      assert_eq!(raiz(36, 2), Ok(6.0));
+      assert_eq!(raiz(-10, 2), Err("El número no puede ser negativo.".to_string()))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod calculadora;
-use calculadora::{sumar as sum, restar as res, multiplicar as mul, division as divi};
+// use calculadora::{sumar as sum, restar as res, multiplicar as mul, division as divi};
+// use calculadora::raiz;
 // mod sumatoria;
 mod validaciones;   // Declaro en módulo validaciones
 use validaciones as vali; // Renombro el módulo validaciones por "vali"
@@ -9,6 +10,6 @@ fn main() {
   num = vali::int();
   let seg: i32 = vali::int();
   println!("Elementos; primero: {}, segundo: {}",num,seg);
-  println!("Sumar: {}\nRestar: {}\nMultiplicar: {}\nDividir: {:?}",sum(num,seg),res(num,seg),mul(num,seg),divi(num,seg))
+  // println!("Sumar: {}\nRestar: {}\nMultiplicar: {}\nDividir: {:?}",sum(num,seg),res(num,seg),mul(num,seg),divi(num,seg))
   // println!("La suma de pares positivos hasta {} es: {}", numero, sumar::pares_n(numero));
 }
